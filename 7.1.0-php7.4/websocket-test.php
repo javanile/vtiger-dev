488272903e5fd44d2c50e07cd63d959d8efbcd3b
francesco <info.francescobianco@gmail.com>
2023-10-10 19:34:25 +0200

    Introducing profiler

--- 7.1.0-php7.4/websocket-test.php
+++ 7.1.0-php7.4/websocket-test.php
@@ -52,7 +52,7 @@ if (isset($_GET['test']) && $_GET['test'] == 'test') {
 </head>
 <body>
 <form>
-    connection to: <input type="text" id="endpoint" name="endpoint" value="ws://localhost:8080/websocket-test.php"  style="width: 200px" ><br>
+    connection to: <input type="text" id="endpoint" name="endpoint" value="ws://localhost:8088/websocket-52000"  style="width: 200px" ><br>
 </form>
 <input type="button" onclick="connectToWS()" value="connect to WebSocket endpoint" /><br><br>
 <form>
@@ -66,74 +66,153 @@ EOT;
     exit;
 }
 
-// Verifica se è stata ricevuta una richiesta WebSocket
-if (isset($_SERVER['HTTP_CONNECTION']) && strtolower($_SERVER['HTTP_CONNECTION']) == 'upgrade' &&
-    isset($_SERVER['HTTP_UPGRADE']) && strtolower($_SERVER['HTTP_UPGRADE']) == 'websocket' &&
-    isset($_SERVER['HTTP_SEC_WEBSOCKET_KEY'])) {
 
-    // Accetta la connessione WebSocket
-    header('Connection: Upgrade');
-    header('Upgrade: websocket');
+$host = $argv[1] ?? '0.0.0.0';
+$port = $argv[2] ?? 52000;
 
-    // Calcola la chiave di accettazione SHA-1
-    $key = $_SERVER['HTTP_SEC_WEBSOCKET_KEY'];
-    $acceptKey = base64_encode(pack('H*', sha1($key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
+define('WEBSOCKET_HOST', $host);
+define('WEBSOCKET_PORT', $port);
 
-    // Invia l'intestazione di conferma WebSocket
-    header('Sec-WebSocket-Accept: ' . $acceptKey);
+class WebSocketServer
+{
+    function send($message) {
+        global $clientSocketArray;
+        $messageLength = strlen($message);
+        foreach($clientSocketArray as $clientSocket)
+        {
+            @socket_write($clientSocket,$message,$messageLength);
+        }
+        return true;
+    }
 
-    // Imposta il buffer di output su non bufferizzato
-    ob_implicit_flush(true);
+    function unseal($socketData) {
+        $length = ord($socketData[1]) & 127;
+        if($length == 126) {
+            $masks = substr($socketData, 4, 4);
+            $data = substr($socketData, 8);
+        }
+        elseif($length == 127) {
+            $masks = substr($socketData, 10, 4);
+            $data = substr($socketData, 14);
+        }
+        else {
+            $masks = substr($socketData, 2, 4);
+            $data = substr($socketData, 6);
+        }
+        $socketData = "";
+        for ($i = 0; $i < strlen($data); ++$i) {
+            $socketData .= $data[$i] ^ $masks[$i%4];
+        }
+        return $socketData;
+    }
 
-    // Leggi i dati WebSocket
-    while (true) {
-        $message = websocket_read();
-        if ($message === false) {
-            break; // Connessione chiusa
+    function seal($socketData) {
+        $b1 = 0x80 | (0x1 & 0x0f);
+        $length = strlen($socketData);
+
+        if($length <= 125)
+            $header = pack('CC', $b1, $length);
+        elseif($length > 125 && $length < 65536)
+            $header = pack('CCn', $b1, 126, $length);
+        elseif($length >= 65536)
+            $header = pack('CCNN', $b1, 127, $length);
+        return $header.$socketData;
+    }
+
+    function doHandshake($received_header,$client_socket_resource, $host_name, $port) {
+        $headers = array();
+        $lines = preg_split("/\r\n/", $received_header);
+        foreach($lines as $line)
+        {
+            $line = chop($line);
+            if(preg_match('/\A(\S+): (.*)\z/', $line, $matches))
+            {
+                $headers[$matches[1]] = $matches[2];
+            }
         }
 
-        // Esegui l'elaborazione del messaggio
-        $response = "Hai inviato: " . $message;
+        $secKey = $headers['Sec-WebSocket-Key'];
+        $secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
+        $buffer  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
+            "Upgrade: websocket\r\n" .
+            "Connection: Upgrade\r\n" .
+            "WebSocket-Origin: $host_name\r\n" .
+            "WebSocket-Location: ws://$host_name:$port/demo/shout.php\r\n".
+            "Sec-WebSocket-Accept:$secAccept\r\n\r\n";
+        socket_write($client_socket_resource,$buffer,strlen($buffer));
+    }
 
-        // Invia una risposta WebSocket
-        websocket_write($response);
+    function newConnectionACK($client_ip_address)
+    {
+        $message = 'New client ' . $client_ip_address.' joined';
+        $messageArray = array('message'=>$message,'message_type'=>'chat-connection-ack');
+        $ACK = $this->seal(json_encode($messageArray));
+        return $ACK;
+    }
+
+    function connectionDisconnectACK($client_ip_address) {
+        $message = 'Client ' . $client_ip_address.' disconnected';
+        $messageArray = array('message'=>$message,'message_type'=>'chat-connection-ack');
+        $ACK = $this->seal(json_encode($messageArray));
+        return $ACK;
+    }
+
+    function createChatBoxMessage($chat_user,$chat_box_message) {
+        $message = $chat_user . ": " . $chat_box_message . "\n";
+        $messageArray = array('message'=>$message,'message_type'=>'chat-box-html');
+        $chatMessage = $this->seal(json_encode($messageArray));
+        return $chatMessage;
     }
 }
 
-function websocket_read() {
-    $data = fread(STDIN, 8192);
-    if ($data === false || feof(STDIN)) {
-        return false;
+$webSocketServer = new WebSocketServer();
+
+$socketResource = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_set_option($socketResource, SOL_SOCKET, SO_REUSEADDR, 1);
+socket_bind($socketResource, 0, WEBSOCKET_PORT);
+socket_listen($socketResource);
+
+$null = null;
+$clientSocketArray = array($socketResource);
+while (true) {
+    $newSocketArray = $clientSocketArray;
+    socket_select($newSocketArray, $null, $null, 0, 10);
+
+    if (in_array($socketResource, $newSocketArray)) {
+        $newSocket = socket_accept($socketResource);
+        $clientSocketArray[] = $newSocket;
+
+        $header = socket_read($newSocket, 1024);
+        $webSocketServer->doHandshake($header, $newSocket, WEBSOCKET_HOST, WEBSOCKET_PORT);
+
+        socket_getpeername($newSocket, $client_ip_address);
+        $connectionACK = $webSocketServer->newConnectionACK($client_ip_address);
+
+        $webSocketServer->send($connectionACK);
+
+        $newSocketIndex = array_search($socketResource, $newSocketArray);
+        unset($newSocketArray[$newSocketIndex]);
     }
 
-    $length = ord($data[1]) & 127;
-    if ($length == 126) {
-        $masks = substr($data, 4, 4);
-        $data = substr($data, 8);
-    } elseif ($length == 127) {
-        $masks = substr($data, 10, 4);
-        $data = substr($data, 14);
-    } else {
-        $masks = substr($data, 2, 4);
-        $data = substr($data, 6);
-    }
+    foreach ($newSocketArray as $newSocketArrayResource) {
+        while(socket_recv($newSocketArrayResource, $socketData, 1024, 0) >= 1){
+            $socketMessage = $webSocketServer->unseal($socketData);
+            $messageObj = json_decode($socketMessage);
 
-    $message = '';
-    for ($i = 0; $i < strlen($data); $i++) {
-        $message .= $data[$i] ^ $masks[$i % 4];
-    }
+            $chat_box_message = $webSocketServer->createChatBoxMessage($messageObj->chat_user, $messageObj->chat_message);
+            $webSocketServer->send($chat_box_message);
+            break 2;
+        }
 
-    return $message;
+        $socketData = @socket_read($newSocketArrayResource, 1024, PHP_NORMAL_READ);
+        if ($socketData === false) {
+            socket_getpeername($newSocketArrayResource, $client_ip_address);
+            $connectionACK = $webSocketServer->connectionDisconnectACK($client_ip_address);
+            $webSocketServer->send($connectionACK);
+            $newSocketIndex = array_search($newSocketArrayResource, $clientSocketArray);
+            unset($clientSocketArray[$newSocketIndex]);
+        }
+    }
 }
 
-function websocket_write($data) {
-    $length = strlen($data);
-
-    if ($length <= 125) {
-        fwrite(STDOUT, "\x81" . chr($length) . $data);
-    } elseif ($length <= 65535) {
-        fwrite(STDOUT, "\x81\x7E" . pack("n", $length) . $data);
-    } else {
-        fwrite(STDOUT, "\x81\x7F" . pack("NN", 0, $length) . $data);
-    }
-}
+socket_close($socketResource);
